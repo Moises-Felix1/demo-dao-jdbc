46d0852df3139de14f3c991371aae931b5ad69bb
Moises Felix <moises012felix@gmail.com>
2024-06-15 15:39:37 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller slr = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+		System.out.println(slr);
 
 	}
 
